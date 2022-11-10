de8a94de86f1103fe913884ac601d3e341708631
David Bui <ducbm0512@gmail.com>
2022-11-10 10:44:12 +0700

    [#4] - Create AppRouter
    - Rename NimbleSurveyApp to SurveysApp
    - Rename SplashScreen to SplashView

---
+++ Surveys/Sources/Application/SurveysApp.swift
@@ -0,0 +1,37 @@
+//
+//  SurveysApp.swift
+//  Surveys
+//
+//  Created by David Bui on 04/11/2022.
+//  Copyright © 2022 Nimble. All rights reserved.
+//
+
+import Firebase
+import SwiftUI
+
+@main
+struct SurveysApp: App {
+
+    @StateObject var appRouter = AppRouter()
+
+    init() {
+        FirebaseApp.configure()
+    }
+}
+
+extension SurveysApp {
+
+    @ViewBuilder var rootView: some View {
+        NavigationStack {
+            switch appRouter.state {
+            case .splash: SplashView()
+            }
+        }
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            rootView.environmentObject(appRouter)
+        }
+    }
+}
