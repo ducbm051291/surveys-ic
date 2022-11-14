a08aa21a44296fe960fd16b427d77cab22831b03
David Bui <ducbm0512@gmail.com>
2022-11-14 13:40:45 +0700

    [#19] - Create Localizable strings
    - Create NetworkAPIError
    - Create NetworkAPIProtocol
    - Create NetworkAPI
    - Update typealiases
    - Remove redundant files
    - Update Podfile: Add JSONAPIMapper

--- Surveys/Sources/Supports/Helpers/Typealiases/Typealiases.swift
+++ Surveys/Sources/Supports/Helpers/Typealiases/Typealiases.swift
@@ -3,7 +3,11 @@
 //  Typealiases.swift
 //
 
+import Combine
 import UIKit
 
 typealias AlertCompletion = (UIAlertAction) -> Void
 typealias Asset = R.image
+typealias CancelBag = Set<AnyCancellable>
+typealias Localize = R.string.localizable
+typealias Observable<T> = AnyPublisher<T, Error>
