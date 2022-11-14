a0e57d5b9df5be13c5f7f7b2188095522ea1e4f7
David Bui <ducbm0512@gmail.com>
2022-11-14 13:40:45 +0700

    [#19] - Create Localizable strings
    - Create NetworkAPIError
    - Create NetworkAPIProtocol
    - Create NetworkAPI
    - Update typealiases
    - Remove redundant files
    - Update Podfile: Add JSONAPIMapper

---
+++ Surveys/Sources/Data/NetworkAPI/Core/NetworkAPIProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkAPIProtocol.swift
+//  Surveys
+//
+//  Created by David Bui on 14/11/2022.
+//  Copyright © 2022 Nimble. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkAPIProtocol {
+}
