2cc5c21c0c1aa40be78276d3c0b3b37be1ad76b3
David Bui <ducbm0512@gmail.com>
2022-11-16 13:40:24 +0700

    [#19] - Improve code

--- Surveys/Sources/Data/NetworkAPI/Core/RequestConfiguration.swift
+++ Surveys/Sources/Data/NetworkAPI/Core/RequestConfiguration.swift
@@ -37,7 +37,7 @@ extension RequestConfiguration: TargetType {
         }
     }
 
-    var headers: [String: String]? {
+    var headers: HTTPHeaders? {
         ["Accept": "application/json"]
     }
 }
