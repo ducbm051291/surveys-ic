f6770830117af1801aa50c5e2c9f3a8b30eb88c7
David Bui <ducbm0512@gmail.com>
2022-11-17 15:37:47 +0700

    [#8] - Update Keychain
    - Create SessionRepository
    - Create StoreTokenUseCase
    - Create Resolver+Domain extension and Resolver+ResolverRegistering to register injection services, domains, and data
    - Create extension for publisher to convert to Driver and Observable
    - Update typealias Driver

--- Surveys/Sources/Injection/Resolver+Data.swift
+++ Surveys/Sources/Injection/Resolver+Data.swift
@@ -16,6 +16,7 @@ extension Resolver {
 
     private static func registerRepositories() {
         register(AuthenticationRepositoryProtocol.self) { AuthenticationRepository() }
+        register(SessionRepositoryProtocol.self) { SessionRepository() }
     }
 
     private static func registerServices() {
