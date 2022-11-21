d6e089eb019784a46ee3d024978406fb8df57922
David Bui <ducbm0512@gmail.com>
2022-11-21 10:39:59 +0700

    [#5] - Update using ColorSet

---
+++ Surveys/Sources/Supports/Extensions/SwiftUI/ColorResource+Color.swift
@@ -0,0 +1,17 @@
+//
+//  ColorResource+Color.swift
+//  Surveys
+//
+//  Created by David Bui on 21/11/2022.
+//  Copyright © 2022 Nimble. All rights reserved.
+//
+
+import Rswift
+import SwiftUI
+
+extension ColorResource {
+
+    var color: Color {
+        Color(name)
+    }
+}
