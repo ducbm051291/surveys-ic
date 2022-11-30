4b8ac5e26f25d86c28986153f6cab6ee2e5f270e
David Bui <ducbm0512@gmail.com>
2022-11-30 10:34:05 +0700

    [#70] - Update example for .env file
    - Update Constants API base on environment
    - Update compilation conditions

--- Surveys/Sources/Constants/Constants+API.swift
+++ Surveys/Sources/Constants/Constants+API.swift
@@ -8,6 +8,12 @@ import SurveysKeys
 extension Constants.API {
 
     static let baseURL: String = "https://\((try? Environment.value(for: .apiBaseURL)) ?? "")/"
-    static let clientId: String = SurveysKeys.Keys.Release().clientId
-    static let clientSecret: String = SurveysKeys.Keys.Release().clientSecret
+
+    #if STAGING
+        static let clientId: String = SurveysKeys.Keys.Debug().clientId
+        static let clientSecret: String = SurveysKeys.Keys.Debug().clientSecret
+    #elseif PRODUCTION
+        static let clientId: String = SurveysKeys.Keys.Release().clientId
+        static let clientSecret: String = SurveysKeys.Keys.Release().clientSecret
+    #endif
 }
