36451ea314e73426f9bbab957e049c558e6f1d3f
David Bui <ducbm0512@gmail.com>
2023-01-03 13:06:36 +0700

    [#27] - Create AnswerSubmittedView
    - Add Lottie in Podfile for Animation
    - Create LottieView to contain animation
    - Update Localizable strings and Lottie successful animation

---
+++ Surveys/Sources/Presentation/Modules/AnswerSubmittedView/AnswerSubmittedView.swift
@@ -0,0 +1,48 @@
+//
+//  AnswerSubmittedView.swift
+//  Surveys
+//
+//  Created by David Bui on 03/01/2023.
+//  Copyright © 2023 Nimble. All rights reserved.
+//
+
+import Kingfisher
+import SwiftUI
+
+struct AnswerSubmittedView: View {
+
+    @EnvironmentObject private var navigator: Navigator
+
+    var body: some View {
+        setUpView()
+    }
+
+    private func setUpView(isLoading: Bool = false) -> some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                VStack(alignment: .center) {
+                    LottieView(lottieFile: "successful")
+                        .frame(
+                            width: 200.0,
+                            height: 200.0
+                        )
+                    Text(Localize.aswerSubmittedViewThanksText())
+                        .modifier(TextTitleModifier(titleSize: .xLarge))
+                        .lineLimit(2)
+                        .multilineTextAlignment(.center)
+                }
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct AnswerSubmittedViewPreview: PreviewProvider {
+
+    static var previews: some View {
+        AnswerSubmittedView()
+    }
+}
