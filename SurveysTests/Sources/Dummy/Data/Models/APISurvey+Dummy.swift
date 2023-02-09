4ced5fb8136e9bddd0885609200c4ec00fad4b20
David Bui <ducbm0512@gmail.com>
2023-02-09 09:18:28 +0700

    [#26] - Update APISurvey Dummy

--- SurveysTests/Sources/Dummy/Data/Models/APISurvey+Dummy.swift
+++ SurveysTests/Sources/Dummy/Data/Models/APISurvey+Dummy.swift
@@ -11,10 +11,68 @@
 extension APISurvey {
 
     static let dummy = APISurvey(
-        id: "d5de6a8f8f5f1cfe51bc",
-        type: "survey_simple",
-        title: "Scarlett Bangkok",
-        description: "We'd love ot hear from you!",
-        coverImageUrl: "https://dhdbhh0jsld0o.cloudfront.net/m/1ea51560991bcb7d00d0_"
+        id: "1",
+        type: "customer satisfaction",
+        title: "How was your experience with us?",
+        description: "We would love to hear your feedback.",
+        coverImageUrl: "https://example.com/cover-image.jpg",
+        questionList: [
+            APISurvey.APIQuestion(
+                id: "1",
+                type: "single-choice",
+                displayOrder: 1,
+                displayType: .choice,
+                text: "How would you rate our customer service?",
+                pick: .one,
+                coverImageUrl: "https://example.com/cover-image.jpg",
+                answerList: [
+                    APISurvey.APIQuestion.APIAnswer(
+                        id: "1",
+                        type: "option",
+                        displayOrder: 1,
+                        text: "Excellent",
+                        inputMaskPlaceholder: nil
+                    ),
+                    APISurvey.APIQuestion.APIAnswer(
+                        id: "2",
+                        type: "option",
+                        displayOrder: 2,
+                        text: "Good",
+                        inputMaskPlaceholder: nil
+                    ),
+                    APISurvey.APIQuestion.APIAnswer(
+                        id: "3",
+                        type: "option",
+                        displayOrder: 3,
+                        text: "Average",
+                        inputMaskPlaceholder: nil
+                    ),
+                    APISurvey.APIQuestion.APIAnswer(
+                        id: "4",
+                        type: "option",
+                        displayOrder: 4,
+                        text: "Poor",
+                        inputMaskPlaceholder: nil
+                    ),
+                    APISurvey.APIQuestion.APIAnswer(
+                        id: "5",
+                        type: "option",
+                        displayOrder: 5,
+                        text: "Very Poor",
+                        inputMaskPlaceholder: nil
+                    )
+                ]
+            ),
+            APISurvey.APIQuestion(
+                id: "2",
+                type: "open-ended",
+                displayOrder: 2,
+                displayType: .textfield,
+                text: "What can we do to improve?",
+                pick: .one,
+                coverImageUrl: "",
+                answerList: nil
+            )
+        ]
     )
 }
